1ca54157e82cbf9419c475caef3e3d22ef6d1169
Arathok <36503011+Arathok@users.noreply.github.com>
2022-03-12 23:10:56 +0100

    Update RenameAction.java

--- src/org/arathok/wurmunlimited/mods/renamePets/RenameAction.java
+++ src/org/arathok/wurmunlimited/mods/renamePets/RenameAction.java
@@ -7,17 +7,20 @@ import com.wurmonline.server.behaviours.Action;
 import com.wurmonline.server.behaviours.ActionEntry;
 import com.wurmonline.server.creatures.Creature;
 import com.wurmonline.server.creatures.Creatures;
+import com.wurmonline.server.creatures.DbCreatureStatus;
 import com.wurmonline.server.creatures.NoSuchCreatureException;
 import com.wurmonline.server.items.Item;
 import com.wurmonline.server.villages.Village;
 import com.wurmonline.server.zones.VirtualZone;
 import com.wurmonline.server.zones.VolaTile;
 import com.wurmonline.server.zones.Zones;
+import org.gotti.wurmunlimited.modloader.ReflectionUtil;
 import org.gotti.wurmunlimited.modsupport.actions.ActionEntryBuilder;
 import org.gotti.wurmunlimited.modsupport.actions.ActionPerformer;
 import org.gotti.wurmunlimited.modsupport.actions.ActionPropagation;
 import org.gotti.wurmunlimited.modsupport.actions.ModActions;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.logging.Level;
 
 public class RenameAction implements ActionPerformer
@@ -74,11 +77,24 @@ public class RenameAction implements ActionPerformer
                     if (!source.getInscription().getInscription().isEmpty()) {
                         performer.getCommunicator().sendSafeServerMessage("you rename the "+target.getName()+" to "+source.getInscription().getInscription());
                         RenamePets.logger.log(Level.INFO,performer.getName()+" renamed" +target.getName() +" to "+source.getInscription().getInscription() );
+                        String newName = source.getInscription().getInscription();
                         try {
-                            Creatures.getInstance().getCreature(target.getWurmId()).setName(source.getInscription().getInscription());
+                            Creatures.getInstance().getCreature(target.getWurmId()).setName(newName);
                         } catch (NoSuchCreatureException e) {
+                            RenamePets.logger.log(Level.INFO, "no such creature!");
                             e.printStackTrace();
                         }
+
+                        try {
+                            ReflectionUtil.callPrivateMethod(target.getStatus(), ReflectionUtil.getMethod(DbCreatureStatus.class, "saveCreatureName", new Class[]{String.class}), newName);
+                        } catch (IllegalAccessException e) {
+                            e.printStackTrace();
+                        } catch (InvocationTargetException e) {
+                            e.printStackTrace();
+                        } catch (NoSuchMethodException e) {
+                            e.printStackTrace();
+                        }
+
                         target.refreshVisible();
                         Items.destroyItem(source.getWurmId());
 
